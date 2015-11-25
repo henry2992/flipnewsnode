10352fb63739e423d2197ecc3cc49c7246f348f5
Bruno Godoy Martins Correa <brunogmc@gmail.com>
2015-11-24 22:38:47 -0600

    Revert "Nodejsversion"

--- flipnews/app.js
+++ flipnews/app.js
@@ -7,14 +7,12 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var news = require('./routes/news');
 
 var app = express();
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views', 'pages'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/app', news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- flipnews/routes/index.js
+++ flipnews/routes/index.js
@@ -2,20 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/index.html', function(req, res, next) {
-  res.render('index');
-});
-
-router.get('/signup.html', function(req, res, next) {
-  res.render('signup');
-});
-
-router.get('/about.html', function(req, res, next) {
-  res.render('about');
-});
-
-router.get('/profile.html', function(req, res, next) {
-  res.render('profile');
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
